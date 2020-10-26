84840b7f9a06fb5d79763533aedec02a0b458fd1
David Martinez Marti <deavidsedice@gmail.com>
2020-10-26 22:03:54 +0000

    Next iteration

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,24 @@
 mod icmp;
 mod transport;
+use std::time::Duration;
 
 #[macro_use]
 extern crate log;
 extern crate env_logger;
 
 fn main() {
-    use pnet::transport::icmp_packet_iter;
-    use pnet::transport::transport_channel;
     info!("Program start");
     let addr = icmp::ipaddr("192.168.0.1").unwrap();
-    let (mut tx, mut rx) = match transport_channel(65536, icmp::protocol()) {
-        Ok((tx, rx)) => (tx, rx),
-        Err(e) => panic!(e.to_string()),
-    };
-    let pid: u16 = (std::process::id() % 65536) as u16;
-    let seqn = 1;
-    let p = icmp::PacketData::new(seqn, pid, addr).send(&mut tx);
-    dbg!(p);
+    let addr2 = icmp::ipaddr("8.8.4.4").unwrap();
+    let interval = Duration::from_millis(5);
 
-    let mut iter = icmp_packet_iter(&mut rx);
-    loop {
-        match iter.next() {
-            Ok((packet, addr)) => {
-                let packet = icmp::PacketData::parse(packet, addr);
-                if packet.ident == pid {
-                    dbg!(packet);
-                }
-            }
-            Err(e) => {
-                error!("An error occurred while reading: {}", e);
-            }
-        }
-    }
+    let mut t = transport::Comms::new();
+    t.add_destination(addr, interval);
+    t.add_destination(addr2, interval);
+    t.send_all();
+    t.send_all();
+    std::thread::sleep(Duration::from_millis(100));
+    let (vpck, err) = t.read(interval);
+    dbg!(vpck);
+    dbg!(err);
 }
--- src/transport.rs
+++ src/transport.rs
@@ -7,6 +7,7 @@ use pnet::transport::{TransportReceiver, TransportSender};
 use rand::Rng;
 use std::net::IpAddr;
 use std::time::Duration;
+use std::time::Instant;
 
 #[derive(Debug)]
 pub struct Destination {
@@ -26,6 +27,10 @@ impl Destination {
             ident: rand::thread_rng().gen(),
             packets: vec![],
         }
+    }
+
+    pub fn recv(&mut self, packet: icmp::PacketData) {
+        dbg!(self, packet);
     }
 }
 
@@ -52,25 +57,60 @@ impl Comms {
         }
     }
 
+    pub fn add_destination(&mut self, addr: IpAddr, interval: Duration) {
+        self.dest.push(Destination::new(addr, interval))
+    }
+
+    /// Sends a ping for each destination
+    pub fn send_all(&mut self) {
+        for dest in &mut self.dest {
+            let packet = icmp::PacketData::new(dest.seq, dest.ident, dest.addr).send(&mut self.tx);
+            dest.seq += 1; // WARN: Should wrap-around
+            dest.packets.push(packet);
+        }
+    }
+
+    pub fn _delay() {
+        /*
+        The delay could be something evenly spaced. Maybe the formula of:
+
+        1/delay = 1/delay1 + 1/delay2 + ...
+        OR
+        HZ = Hz1 + Hz2 + Hz3 + ...
+
+        Could get us something evenly spaced. The idea being that if there are
+        4 destinations at 40ms, we send one each 10ms, instead of doing all four
+        at once every 40ms.
+        */
+    }
+
     /// Waits for timeout for icmp packets, then subsequentially
     /// keeps reading until the buffer is exhausted. If an error occurs, will
     /// return the packets read so far, plus the error. Filters those packets that
     /// do not match the ident variable
     pub fn read(&mut self, timeout: Duration) -> (Vec<icmp::PacketData>, Option<std::io::Error>) {
+        // TODO: Rename to recv_all  & change return to Result::Ok(None)
         let mut iter = icmp_packet_iter(&mut self.rx);
-        let mut timeout = timeout;
+        let mut next_timeout = timeout;
         let mut vec: Vec<icmp::PacketData> = vec![];
+        let starttime = Instant::now();
         loop {
-            match iter.next_with_timeout(timeout) {
+            match iter.next_with_timeout(next_timeout) {
                 Ok(data) => {
                     if let Some((packet, addr)) = data {
                         let packet = icmp::PacketData::parse(packet, addr);
-                        for dest in &self.dest {
+                        for dest in &mut self.dest {
                             if packet.ident == dest.ident {
-                                vec.push(packet.clone());
+                                vec.push(packet.clone()); // DELETE THIS
+                                dest.recv(packet.clone());
                             }
                         }
-                        timeout = Duration::from_micros(0);
+                        let elapsed = starttime.elapsed();
+                        if elapsed + Duration::from_micros(1) > timeout {
+                            break;
+                        }
+                        // TODO: If all packets are already consumed, return early!
+                        next_timeout = timeout - elapsed;
                     } else {
                         break;
                     }
