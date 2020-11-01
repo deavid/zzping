5cbddf43761bb8ca62a8cd81b2da6a53cff24d16
David Martinez Marti <deavidsedice@gmail.com>
2020-11-01 12:34:36 +0000

    add packet loss to UI

--- pingmongui/src/main.rs
+++ pingmongui/src/main.rs
@@ -1,7 +1,8 @@
 use iced::{
-    canvas, executor, slider, Application, Canvas, Color, Column, Command, Element, Length, Point,
-    ProgressBar, Settings, Slider, Subscription,
+    canvas, executor, Application, Canvas, Color, Column, Command, Element, Length, Point,
+    Settings, Subscription,
 };
+// use iced::{ProgressBar,  slider,Slider}
 use rand::Rng;
 use std::net::UdpSocket;
 use std::time::{Duration, Instant};
@@ -30,7 +31,7 @@ struct UdpStats {
     pub inflight_count: u16,
     pub avg_time_us: u32,
     pub last_pckt_ms: u32,
-    pub packet_loss_x1000: u32,
+    pub packet_loss_x100_000: u32,
 }
 
 impl UdpStats {
@@ -39,7 +40,7 @@ impl UdpStats {
         let inflight_count: u16;
         let avg_time_us: u32;
         let last_pckt_ms: u32;
-        let packet_loss_x1000: u32;
+        let packet_loss_x100_000: u32;
 
         let len = rmp::decode::read_array_len(&mut v)?;
         if len != 5 {
@@ -53,14 +54,14 @@ impl UdpStats {
         inflight_count = rmp::decode::read_u16(&mut v)?;
         avg_time_us = rmp::decode::read_u32(&mut v)?;
         last_pckt_ms = rmp::decode::read_u32(&mut v)?;
-        packet_loss_x1000 = rmp::decode::read_u32(&mut v)?;
+        packet_loss_x100_000 = rmp::decode::read_u32(&mut v)?;
 
         Ok(Self {
             addr,
             inflight_count,
             avg_time_us,
             last_pckt_ms,
-            packet_loss_x1000,
+            packet_loss_x100_000,
         })
     }
 }
@@ -68,14 +69,18 @@ impl UdpStats {
 pub fn main() {
     PingmonGUI::run(Settings {
         antialiasing: true,
+        window: iced::window::Settings {
+            size: (1600, 400),
+            ..iced::window::Settings::default()
+        },
         ..Settings::default()
     })
 }
 
 #[derive(Default)]
 struct PingmonGUI {
-    value: f32,
-    progress_bar_slider: slider::State,
+    // value: f32,
+    // progress_bar_slider: slider::State,
     graph: LatencyGraph,
     graph_canvas: canvas::layer::Cache<LatencyGraph>,
     socket: Option<UdpSocket>,
@@ -107,7 +112,8 @@ impl PingmonGUI {
 
 #[derive(Debug)]
 struct LatencyGraph {
-    points: Vec<u32>,
+    latency_us: Vec<u32>,
+    packet_loss_x100_000: Vec<u32>,
     current: Instant,
 }
 
@@ -123,7 +129,8 @@ fn _rand_num() -> u32 {
 impl LatencyGraph {
     fn new() -> Self {
         Self {
-            points: vec![],
+            latency_us: vec![],
+            packet_loss_x100_000: vec![],
             current: Instant::now(),
         }
     }
@@ -131,12 +138,14 @@ impl LatencyGraph {
         let mut modified = false;
         for s in stats {
             if s.addr == "192.168.0.1" {
-                self.points.push(s.avg_time_us);
+                self.latency_us.push(s.avg_time_us);
+                self.packet_loss_x100_000.push(s.packet_loss_x100_000);
                 modified = true;
             }
         }
-        while self.points.len() >= 200 {
-            self.points.remove(0);
+        while self.latency_us.len() >= 2000 {
+            self.latency_us.remove(0);
+            self.packet_loss_x100_000.remove(0);
             modified = true;
         }
         // self.points.push(rand_num());
@@ -160,31 +169,78 @@ impl canvas::Drawable for LatencyGraph {
     fn draw(&self, frame: &mut canvas::Frame) {
         use canvas::{Path, Stroke};
         let space = Path::rectangle(Point::new(0.0, 0.0), frame.size());
-        let botright = Point::new(frame.width(), frame.height());
+        let right = frame.width();
         let bottom = frame.height();
+        let botright = Point::new(right, bottom);
         let green_stroke = Stroke {
-            width: 3.0,
+            width: 1.0,
             color: Color::from_rgba8(0, 255, 0, 0.3),
             ..Stroke::default()
         };
         let red_stroke = Stroke {
-            width: 3.0,
+            width: 1.0,
             color: Color::from_rgba8(255, 0, 0, 0.5),
+            ..Stroke::default()
+        };
+        let black_stroke1 = Stroke {
+            width: 3.0,
+            color: Color::from_rgba8(0, 0, 0, 0.1),
+            ..Stroke::default()
+        };
+        let black_stroke2 = Stroke {
+            width: 3.0,
+            color: Color::from_rgba8(0, 0, 0, 0.2),
             ..Stroke::default()
         };
 
         frame.fill(&space, Color::from_rgba8(100, 100, 100, 1.0));
-        if self.points.is_empty() {
+        if self.latency_us.is_empty() {
             let line = canvas::Path::line(Point::new(0.0, 0.0), botright);
             frame.stroke(&line, red_stroke);
             return;
         }
-        let max = self.points.iter().max().unwrap();
-        let len = self.points.len();
+        let ms: f32 = 1000.0;
+        let max = self
+            .latency_us
+            .iter()
+            .filter(|x| **x < 2000000)
+            .max()
+            .unwrap();
+        // let len = self.points.len();
+        let len = 2000;
         let sx = frame.width() / len as f32;
         let sy = (frame.height() / *max as f32) * 0.8;
+
+        let y3ms = bottom - 3.0 * ms * sy;
+        let y10ms = bottom - 10.0 * ms * sy;
+        let y30ms = bottom - 30.0 * ms * sy;
+        let y100ms = bottom - 100.0 * ms * sy;
+        if y3ms > 0.0 {
+            frame.stroke(
+                &canvas::Path::line(Point::new(0.0, y3ms), Point::new(right, y3ms)),
+                black_stroke1,
+            )
+        }
+        if y10ms > 0.0 {
+            frame.stroke(
+                &canvas::Path::line(Point::new(0.0, y10ms), Point::new(right, y10ms)),
+                black_stroke2,
+            )
+        }
+        if y30ms > 0.0 {
+            frame.stroke(
+                &canvas::Path::line(Point::new(0.0, y30ms), Point::new(right, y30ms)),
+                black_stroke1,
+            )
+        }
+        if y100ms > 0.0 {
+            frame.stroke(
+                &canvas::Path::line(Point::new(0.0, y100ms), Point::new(right, y100ms)),
+                black_stroke2,
+            )
+        }
         let mut oldp: Option<Point> = None;
-        for (n, p) in self.points.iter().enumerate() {
+        for (n, p) in self.latency_us.iter().enumerate() {
             let x = n as f32 * sx;
             let y = bottom - *p as f32 * sy;
             let point = Point::new(x, y);
@@ -200,12 +256,31 @@ impl canvas::Drawable for LatencyGraph {
             }
             oldp = Some(point);
         }
+        // Packet Loss:
+        let sy: f32 = (frame.height() / 100000.0) * 1.0;
+        let mut oldp: Option<Point> = None;
+        for (n, p) in self.packet_loss_x100_000.iter().enumerate() {
+            let x = n as f32 * sx;
+            let y = bottom - *p as f32 * sy;
+            let point = Point::new(x, y);
+            if let Some(oldp) = oldp {
+                let line = canvas::Path::line(oldp, point);
+                frame.stroke(&line, red_stroke);
+            }
+            if n == len - 1 {
+                let x2 = frame.width();
+                let p2 = Point::new(x2, y);
+                let line = canvas::Path::line(point, p2);
+                frame.stroke(&line, red_stroke);
+            }
+            oldp = Some(point);
+        }
     }
 }
 
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    SliderChanged(f32),
+    // SliderChanged(f32),
     Tick(Instant),
     Startup,
 }
@@ -224,13 +299,13 @@ impl Application for PingmonGUI {
     }
 
     fn subscription(&self) -> Subscription<Message> {
-        time::every(std::time::Duration::from_millis(10)).map(Message::Tick)
+        time::every(std::time::Duration::from_millis(200)).map(Message::Tick)
     }
 
     fn update(&mut self, message: Message) -> Command<Message> {
         // dbg!(message);
         match message {
-            Message::SliderChanged(x) => self.value = x,
+            // Message::SliderChanged(x) => self.value = x,
             Message::Tick(instant) => {
                 // println!("Tick: {:#?}", instant);
                 let stats = self.recv_all();
@@ -252,14 +327,14 @@ impl Application for PingmonGUI {
             .push(self.graph_canvas.with(&self.graph));
 
         Column::new()
-            .padding(10)
-            .push(ProgressBar::new(0.0..=100.0, self.value))
-            .push(Slider::new(
-                &mut self.progress_bar_slider,
-                0.0..=100.0,
-                self.value,
-                Message::SliderChanged,
-            ))
+            .padding(5)
+            // .push(ProgressBar::new(0.0..=100.0, self.value))
+            // .push(Slider::new(
+            //     &mut self.progress_bar_slider,
+            //     0.0..=100.0,
+            //     self.value,
+            //     Message::SliderChanged,
+            // ))
             .push(graph)
             .into()
     }
